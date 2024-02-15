bc088706595a660a4fc962c8f1a737635ea0c21f
alemes7 <alemesferreira7@gmail.com>
2024-02-15 10:11:10 -0300

    update

--- app.py
+++ app.py
@@ -9,13 +9,13 @@ CORS(app)
 def homepage():
         return render_template('home.html')
 
-@app.route('/lista')
-def lista():
-    with open('text.csv', 'r') as file:
-            reader = csv.DictReader(file)
-            itens = list(reader)
+@app.route('/produtos')
+def produtos():
+        with open('Text.csv', 'r') as file:
+                reader = csv.DictReader(file)
+                itens = list(reader)
 
-    return render_template('index.html', itens=itens)
+        return render_template('produtos.html', itens=itens)
 
 if __name__ == "__main__":
         app.run(debug=True, host="0.0.0.0")
